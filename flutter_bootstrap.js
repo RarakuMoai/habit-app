1205e8378b0610e325b224f01bd2bbcec3e1c56a
RarakuMoai <RarakuMoai@users.noreply.github.com>
2026-06-08 07:44:44 +0000

    deploy: 577dba7954aa6cc0c1f9984cf5ecec1989101f60

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1434198845" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1257434217" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
